fdf420d13abe92388917d8a3d1c00eac55141e42
paul <paul.doazan@edumedia-sciences.com>
2021-06-18 16:54:16 +0200

    Fix overlay

--- src/Player/Track.jsx
+++ src/Player/Track.jsx
@@ -9,18 +9,18 @@ export default function Track(props) {
   // Refs
   const [duration, setDuration] = useState(0);
   const [displayDescription, setDisplayDuration] = useState(`none`);
-  const [displayWidth, setDisplayWidth] = useState(`95%`);
+  const [displayWidth, setDisplayWidth] = useState(`0%`);
+  const [displayOpacity, setDisplayOpacity] = useState(`0`);
 
   const currentPercentage = duration
-    ? `${(props.trackProgress / duration) * 100}%`
+    ? `${ (props.trackProgress / duration) * 100 }%`
     : "0%";
   const trackStyling = `
-  -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
+  -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${ currentPercentage }, #fff), color-stop(${ currentPercentage }, #777))
 `;
 
   const handleDown = () => {
     props.onMouseDown();
-    setDisplayWidth(`95%`);
   };
 
   const handleChange = (event) => {
@@ -44,27 +44,29 @@ export default function Track(props) {
   }, [duration]);
 
   useEffect(() => {
-    //setDisplayWidth(`100%`);
-  }, []);
+    setDisplayOpacity(1);
+  }, [duration]);
+
+  useEffect(() => {
+    setDisplayWidth('95%');
+  }, [duration]);
 
   return (
-    <div className='input-container'>
+    <div className='input-container' style={{ opacity: displayOpacity }}>
       <input
         type='range'
         value={props.trackProgress}
         step='1'
         min='0'
-        max={duration ? duration : `${duration}`}
+        max={duration ? duration : `${ duration }`}
         className='input-progress'
         onChange={handleChange}
         onMouseDown={handleDown}
         onMouseUp={handleChange}
-        /*onKeyUp={onPressup}*/
         style={{
           background: trackStyling,
           display: displayDescription,
           width: displayWidth,
-          transition: `width 2s`
         }}
       />
       <Track_level_control audio={audio} />
